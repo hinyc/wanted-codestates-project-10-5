4a8e4aa063c5586f02b61c53ecfd945400c946de
juo <hwgiwh271@gmail.com>
2022-03-16 19:26:06 +0900

    [Feat] #5 검색 결과 반응형 UI 구현
    -

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -2,12 +2,31 @@ import React from 'react';
 import styled from 'styled-components';
 import Nav from '../components/assign1/Nav';
 import ImageBox from '../components/assign1/ImageBox';
+import MoreBtn from '../components/assign1/MoreBtn';
 
 function Result(props) {
   return (
     <Container>
       <Nav />
-      <ImageBox />
+      <ResultWrapper>
+        <ImageBox />
+        <ImageBox />
+        <ImageBox />
+        <ImageBox />
+        <ImageBox />
+        <ImageBox />
+        <ImageBox />
+        <ImageBox />
+        <ImageBox />
+        <ImageBox />
+        <ImageBox />
+        <ImageBox />
+        <ImageBox />
+        <ImageBox />
+      </ResultWrapper>
+      <ButtonWrapper>
+        <MoreBtn />
+      </ButtonWrapper>
     </Container>
   );
 }
@@ -16,4 +35,20 @@ const Container = styled.div`
   height: 100%;
   background-color: #fff;
 `;
+const ResultWrapper = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  gap: 4rem;
+  width: 100%;
+  height: auto;
+  padding: 0 3rem;
+`;
 export default Result;
+const ButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 4.2rem;
+  padding-bottom: 1rem;
+`;
